536680a1c1fab0d59d2223505b9bd0141e232b6a
Giovanni Souza <GSMartins999>
2023-08-10 20:36:40 -0300

    StyledComponents-exercicios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardVideo from "./Componentes/CardVideo";
-import "./styles.css";
+import styled from "styled-components";
 
 export default function App() {
   const card1 = {
@@ -9,39 +9,100 @@ export default function App() {
     textoAlternativo: "descrição da imagem"
   };
 
+const Body = styled.body`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`
+
+const TelaInteira = styled.area`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Cabecalho = styled.header`
+  background-color: orange;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 10%;
+`
+
+const Main = styled.main`
+  min-height: 80%;
+  display: flex;
+`
+
+const Nav = styled.nav`
+  flex-basis: 200px;
+  border-right-style: solid;
+  border-right-width: thin;
+`
+
+const Li = styled.li`
+  list-style-type: none;
+`
+
+const PainelDeVideos = styled.section`
+  flex-grow: 1;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  row-gap: 10px;
+  column-gap: 10px;
+  margin: 10px;
+`
+
+const Footer = styled.footer`
+  background: #333b3e;
+  color: white;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  padding: 0 10px;
+
+`
+const H4 = styled.h4`
+  margin-left: 20px;
+
+`
+
   return (
-    <div>
-      <div className="tela-inteira">
-        <header>
+    <Body>
+      <TelaInteira>
+        <Cabecalho>
           <h1>LabeTube</h1>
           <input type="text" placeholder="Busca" id="campoDeBusca" />
-        </header>
+        </Cabecalho>
 
-        <main>
-          <nav className="menu-vertical">
+        <Main>
+          <Nav>
             <ul>
-              <li className="botoes-meunu-vertical">Início</li>
-              <li className="botoes-meunu-vertical">Em alta</li>
-              <li className="botoes-meunu-vertical">Inscrições</li>
+              <Li>Início</Li>
+              <Li >Em alta</Li>
+              <Li>Inscrições</Li>
               <hr />
-              <li className="botoes-meunu-vertical">Originais</li>
-              <li className="botoes-meunu-vertical">Histórico</li>
+              <Li>Originais</Li>
+              <Li>Histórico</Li>
             </ul>
-          </nav>
+          </Nav>
 
-          <section className="painel-de-videos">
+          <PainelDeVideos>
             <CardVideo
               image1={card1.imagemDoVideo}
               titulo={card1.titulo}
               textoAlternativo={card1.textoAlternativo}
             />
-          </section>
-        </main>
+          </PainelDeVideos>
+        </Main>
 
-        <footer>
-          <h4>Oi! Eu moro no footer!</h4>
-        </footer>
-      </div>
-    </div>
+        <Footer>
+          <H4>Oi! Eu moro no footer!</H4>
+        </Footer>
+      </TelaInteira>
+    </Body>
   );
 }
